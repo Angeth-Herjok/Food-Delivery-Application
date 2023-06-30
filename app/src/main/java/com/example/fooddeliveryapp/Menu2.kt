ed3d6bc22b0d56c80fffca1d92d0ab1cf8fab2ca
Angeth-Herjok <angethherjok@gmail.com>
2023-06-30 08:49:09 +0300

    first commit

--- app/src/main/java/com/example/fooddeliveryapp/Menu2.kt
+++ app/src/main/java/com/example/fooddeliveryapp/Menu2.kt
@@ -1,11 +1,29 @@
 package com.example.fooddeliveryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fooddeliveryapp.databinding.ActivityMenu2Binding
+import com.example.fooddeliveryapp.databinding.ActivityReviewsBinding
+import kotlin.collections.Map
 
 class Menu2 : AppCompatActivity() {
+    lateinit var binding: ActivityMenu2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu2)
+        binding= ActivityMenu2Binding.inflate(layoutInflater)
+    }
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+        binding.ivArrow.setOnClickListener {
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSearch.setOnClickListener {
+            val intent= Intent(this, Menu::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/fooddeliveryapp/Reviews.kt
+++ app/src/main/java/com/example/fooddeliveryapp/Reviews.kt
@@ -1,11 +1,29 @@
 package com.example.fooddeliveryapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fooddeliveryapp.databinding.ActivityPaymentBinding
+import com.example.fooddeliveryapp.databinding.ActivityReviewsBinding
+import kotlin.collections.Map
 
 class Reviews : AppCompatActivity() {
+    lateinit var binding: ActivityReviewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reviews)
+        binding= ActivityReviewsBinding.inflate(layoutInflater)
+    }
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+        binding.ivArrow.setOnClickListener {
+            val intent= Intent(this, Track::class.java)
+            startActivity(intent)
+        }
+        binding.ivArrow1.setOnClickListener {
+            val intent= Intent(this, Menu2::class.java)
+            startActivity(intent)
+        }
     }
 }
